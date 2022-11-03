16e08451c7d5eaff9048dfa107dbc632d8fa292a
yantingwei <akkoyan@qq.com>
2022-11-03 16:15:55 +0800

    :white_check_mark: 新增测试

--- src/main/java/com/akko/projectucbackend/ProjectUcBackendApplication.java
+++ src/main/java/com/akko/projectucbackend/ProjectUcBackendApplication.java
@@ -1,9 +1,14 @@
 package com.akko.projectucbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author akko
+ */
 @SpringBootApplication
+@MapperScan("com.akko.projectucbackend.mapper")
 public class ProjectUcBackendApplication {
 
     public static void main(String[] args) {
