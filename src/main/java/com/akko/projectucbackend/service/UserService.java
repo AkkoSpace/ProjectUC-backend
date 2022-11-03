f38770f810920eea88fcad19179cd3b50ee80540
yantingwei <akkoyan@qq.com>
2022-11-03 17:30:19 +0800

    :necktie: 注册用户

--- src/main/java/com/akko/projectucbackend/service/UserService.java
+++ src/main/java/com/akko/projectucbackend/service/UserService.java
@@ -10,4 +10,13 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface UserService extends IService<User> {
 
+    /**
+     * 用户校验
+     *
+     * @param userAccount   用户账号
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
 }
