f3dd6eb6010d0d59f93fb672414947254f934384
yantingwei <akkoyan@qq.com>
2022-11-03 15:08:38 +0800

    :tada: 初始化项目

---
+++ src/main/java/com/akko/projectucbackend/ProjectUcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.akko.projectucbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUcBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectUcBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akko/projectucbackend/ProjectUcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akko.projectucbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
