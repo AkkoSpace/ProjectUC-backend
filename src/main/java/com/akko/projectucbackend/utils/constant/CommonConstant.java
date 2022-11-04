e8d7f7dd26dd369346c0485720344bca5c19f5a3
yantingwei <akkoyan@qq.com>
2022-11-04 15:10:19 +0800

    :technologist: 新增通用常量和用户常量

---
+++ src/main/java/com/akko/projectucbackend/utils/constant/CommonConstant.java
@@ -0,0 +1,21 @@
+package com.akko.projectucbackend.utils.constant;
+
+/**
+ * 通用常量
+ *
+ * @author akko
+ */
+public final class CommonConstant {
+
+    private CommonConstant() {
+        // do nothing
+    }
+
+    /**
+     * 阿拉伯数字 - Integer
+     */
+    public static final Integer FOUR = 4;
+    public static final Integer EIGHT = 8;
+    public static final Integer SIXTEEN = 16;
+
+}
---
+++ src/main/java/com/akko/projectucbackend/utils/constant/UserConstant.java
@@ -0,0 +1,31 @@
+package com.akko.projectucbackend.utils.constant;
+
+/**
+ * 用户常量
+ *
+ * @author akko
+ */
+
+public final class UserConstant {
+
+    private UserConstant() {
+        // do nothing
+    }
+
+    /**
+     * 盐值,用于加密
+     */
+    public static final String SALT = "akko";
+    /**
+     * 用户登录态键
+     */
+    public static final String USER_LOGIN_SESSION_KEY = "userLoginSessionKey";
+    /**
+     * 角色类型
+     * 0 - 管理员
+     * 1 - 默认用户
+     */
+    public static final Integer ADMIN_ROLE_TYPE = 0;
+    public static final Integer DEFAULT_ROLE_TYPE = 1;
+
+}
