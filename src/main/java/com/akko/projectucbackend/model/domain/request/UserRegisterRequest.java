edf757ca94d60bcadeb9eadb58c438d8491d7bc7
yantingwei <akkoyan@qq.com>
2022-11-04 15:14:47 +0800

    :necktie: 用户注册请求

---
+++ src/main/java/com/akko/projectucbackend/model/domain/request/UserRegisterRequest.java
@@ -0,0 +1,30 @@
+package com.akko.projectucbackend.model.domain.request;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 用户注册请求
+ *
+ * @author akko
+ */
+@Data
+public class UserRegisterRequest implements Serializable {
+    /**
+     * 序列化 UID
+     */
+    private static final long serialVersionUID = 3881110054296987441L;
+    /**
+     * 用户账号
+     */
+    private String userAccount;
+    /**
+     * 用户密码
+     */
+    private String userPassword;
+    /**
+     * 校验密码
+     */
+    private String checkPassword;
+}
